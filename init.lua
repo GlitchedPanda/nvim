04fa89fad5106824908c8ea38f6b49aa690f996a
Glitched Panda <82221876+GlitchedPanda@users.noreply.github.com>
2024-07-10 14:42:27 +0200

    Stopped nvim-tree from starting on startup

--- init.lua
+++ init.lua
@@ -140,7 +140,8 @@ require("lspconfig").omnisharp.setup({
 
 local function open_nvim_tree()
   require('nvim-tree.api').tree.open()
-end
+  require('nvim-tree.api').tree.close()
+end 
 
 vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
 
